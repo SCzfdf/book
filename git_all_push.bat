0a42a7b602798aa916bf96e279a86192eb630183
SCzfdf <chen412235679@163.com>
2022-06-29 21:00:54 +0800

    自动提交 2022/06/29 周三 21:00:50.50

--- git_all_push.bat
+++ git_all_push.bat
@@ -4,8 +4,10 @@ git push
 
 cd gitbook
 cd _book
+git init
+git remote add origin git@github.com:SCzfdf/gitbook.git
 git add .
 git commit -m "自动提交 %date% %time%" 
-git push
+git push origin master -f 
 
 ::pause
